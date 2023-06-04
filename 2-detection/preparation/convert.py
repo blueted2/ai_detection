a2189d77412e96a4edc0d5fe43da2ae61544428c
Gregory Holder <gregory.holder76@gmail.com>
2023-06-04 11:50:31 +0200

    Use fixed class ordering and update dataset config

--- 2-detection/preparation/convert.py
+++ 2-detection/preparation/convert.py
@@ -12,30 +12,51 @@ Coco_line = namedtuple(
     "Coco", ["class_", "x_center", "y_center", "width", "height"])
 
 
-def dota_to_coco(line: Dota_line, categories_to_class: dict[str, int], img_size: Tuple[int, int]) -> Coco_line:
-    class_ = categories_to_class[line.category]
+# using a fixed mapping of categories to classes instead of growing the list when a new category is found
+# this should keep the class numbers consistent
+categories = [
+    "small-vehicle",
+    "large-vehicle",
+    "bridge",
+    "tennis-court",
+    "ship",
+    "harbor",
+    "basketball-court",
+    "soccer-ball-field",
+    "swimming-pool"]
+
+
+def dota_to_coco(line: Dota_line, img_size: Tuple[int, int]) -> Coco_line:
+    # find the class number
+    try:
+        class_ = categories.index(line.category)
+    except ValueError:
+        # throw an error if the category is not found
+        raise ValueError(f"Unknown category {line.category}")
+
+    img_width, img_height = img_size
 
     min_x = int(min(line.x1, line.x2, line.x3, line.x4))
     max_x = int(max(line.x1, line.x2, line.x3, line.x4))
     min_y = int(min(line.y1, line.y2, line.y3, line.y4))
     max_y = int(max(line.y1, line.y2, line.y3, line.y4))
 
-    width = max_x - min_x
-    height = max_y - min_y
+    bbox_width = max_x - min_x
+    bbox_height = max_y - min_y
 
-    x_center = min_x + width / 2
-    y_center = min_y + height / 2
+    x_center = min_x + bbox_width / 2
+    y_center = min_y + bbox_height / 2
 
     # normalize
-    x_center /= img_size[0]
-    y_center /= img_size[1]
-    width /= img_size[0]
-    height /= img_size[1]
+    x_center /= img_width
+    y_center /= img_height
+    bbox_width /= img_width
+    bbox_height /= img_height
 
-    c = Coco_line(class_, x_center, y_center, width, height)
+    c = Coco_line(class_, x_center, y_center, bbox_width, bbox_height)
 
     # if anything is negative, print a warning
-    if x_center < 0 or y_center < 0 or width < 0 or height < 0:
+    if x_center < 0 or y_center < 0 or bbox_width < 0 or bbox_height < 0:
         print(f"Warning: Negative value in {line}")
         print(c)
 
@@ -45,39 +66,44 @@ def dota_to_coco(line: Dota_line, categories_to_class: dict[str, int], img_size:
 def main():
     parser = argparse.ArgumentParser(
         description="Convert DOTA labels to COCO labels")
+    
+    parser.add_argument("labels_dir", help="The folder containing the DOTA test labels")
 
-    parser.add_argument("labels", help="The folder containing the DOTA labels")
     parser.add_argument("-o", "--output", help="The folder to output the COCO labels to",
-                        dest="output", default="coco_labels")
+                        dest="output")
+    
     parser.add_argument("-s", "--size", help="The size of the images", dest="img_size",
                         nargs=2, type=int, default=(1024, 1024), metavar=("WIDTH", "HEIGHT"))
 
     args = parser.parse_args()
+    
 
-    labels_folder = args.labels
-    output_labels_folder = args.output
+    # check if the labels folder exists
+    if not os.path.isdir(args.labels_dir):
+        raise ValueError(f"Labels directory {args.labels_dir} does not exist")
+    
+    # check if the output directory exists
+    if os.path.isdir(args.output):
+        raise ValueError(f"Output directory {args.output} already exists")
 
-    # width, height
-    img_size = args.img_size
+    
+    # print a recap of the arguments, such as where the test and train labels are, where the output will be and the image size
+    print("Summary:")
+    print(f"  Labels: {args.labels_dir}")
+    print(f"  Output: {args.output}")
+    print(f"  Image size: {args.img_size[0]}x{args.img_size[1]}")
 
-    # keep track of all categories and their class
-    categories = set()
-
-    # category string to class int
-    category_to_class = {}
-
-    print(f"Converting labels from {labels_folder} to {output_labels_folder}")
 
     # make the output folder if it doesn't exist
-    os.makedirs(output_labels_folder, exist_ok=True)
+    os.makedirs(args.output, exist_ok=True)
 
-    for file in os.listdir(labels_folder):
+    for file in os.listdir(args.labels_dir):
         # open the file and read the lines
-        with open(os.path.join(labels_folder, file), "r") as f:
+        with open(os.path.join(args.labels_dir, file), "r") as f:
             lines = f.read().splitlines()
 
         # open the output file
-        with open(os.path.join(output_labels_folder, file), "w") as f:
+        with open(os.path.join(args.output, file), "w") as f:
             for line in lines:
                 *oobb, category, difficulty = line.split(" ")
                 oobb = [float(x) for x in oobb]
@@ -85,20 +111,19 @@ def main():
                 dota_line = Dota_line(*oobb, category, difficulty)
 
                 if dota_line.category not in categories:
-                    categories.add(dota_line.category)
-                    category_to_class[dota_line.category] = len(categories) - 1
+                    print(f"Warning: Unknown category {dota_line.category}")
 
                 coco_line = dota_to_coco(
-                    dota_line, category_to_class, img_size)
+                    dota_line, (args.img_size[0], args.img_size[1]))
 
                 # write the coco line to the file
                 f.write(
                     f"{coco_line.class_} {coco_line.x_center:.6f} {coco_line.y_center:.6f} {coco_line.width:.6f} {coco_line.height:.6f}\n")
 
     # print all categories with their class
-    # print("Categories:")
-    # for category, class_ in category_to_class.items():
-    #     print(f" {class_}: \"{category}\"")
+    print("names:")
+    for i, category in enumerate(categories):
+        print(f"  {i}: \"{category}\"")
 
 
 if __name__ == "__main__":
